1bb2a2718cb2795e10669067f92535e11decf8b8
Derek <derek.wright@colorado.edu>
2018-03-08 09:53:26 -0700

    Rid of enable bits

--- Logical.v
+++ Logical.v
@@ -1,36 +1,31 @@
-module AND_D(x,y,out,en);
+module AND_D(x,y,out);
    input [3:0] x,y;
    output [3:0] out;
-	input en;
-
-	assign out = x & y;
+   assign out = x & y;
 	
 endmodule // AND_D
 
-module OR_D(x,y,out,en);
+module OR_D(x,y,out);
    input [3:0] x,y;
    output [3:0] out;
-	input en;
 
-	assign out = x | y;
+   assign out = x | y;
 
 endmodule // OR_D
 
-module XOR_D(x,y,out,en);
+module XOR_D(x,y,out);
    input [3:0] x,y;
    output [3:0] out;
-	input en;
 
-	assign out = x ^ y;
+   assign out = x ^ y;
 	
 endmodule // XOR_D
 
-module NOT_D(z,out,en);
+module NOT_D(z,out);
    input [7:0] z;
    output [7:0] out;
-	input en;
-
-	assign out = ~z;
+   
+   assign out = ~z;
  
 endmodule // NOT_D
 
